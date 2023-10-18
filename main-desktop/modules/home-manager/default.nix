a986a07db43a226ace2c976c11f0061e6411d3e9
Kodlak15 <stanlcod15@gmail.com>
2023-10-18 13:23:14 -0700

    Work on desktop flake

---
+++ main-desktop/modules/home-manager/default.nix
@@ -0,0 +1,2 @@
+# Add reusable home-manager modules here
+# This should contain things you would like to share with others, not personal configurations
---
+++ main-desktop/nixos/default.nix
@@ -0,0 +1,135 @@
+{ 
+  inputs, 
+  outputs, 
+  config, 
+  pkgs, 
+  lib, 
+  hyprland, 
+  ... }: {
+  imports =
+    [ 
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      # Import home-manager NixOS module
+      inputs.home-manager.nixosModules.home-manager
+    ];
+
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
+  # Enable flakes and the new cl tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Add home configurations
+  home-manager = {
+    extraSpecialArgs = { inherit inputs outputs; };
+    users = {
+      cody = import ../home-manager/default.nix;
+    };
+  };
+
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Include necessary kernel modules
+  boot.initrd.availableKernelModules = [
+    "cryptd"
+  ];
+
+  # Set up LUKS device
+  boot.initrd.luks.devices = {
+    nixos = {
+      device = lib.mkDefault "/dev/disk/by-partuuid/958c0aa4-f037-c647-9fe2-39daa8f3377c";
+      header = "/dev/disk/by-partuuid/1be33eb7-4534-b24d-83cb-a2510e75cb29";
+      # device = lib.mkDefault "/dev/disk/by-partuuid/1eca85cd-d72c-3d46-af34-811fa6657c6e";
+      # header = "/dev/disk/by-partuuid/877a327f-c0c6-7748-a9b2-f6412d218793";
+      preLVM = true;
+    };
+  };
+
+  # Set up networking
+  networking.hostName = "nixos-test-vm";
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "US/Pacific";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+    useXkbConfig = true; # use xkbOptions in tty.
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # TODO this should probably be handled in home-manager directory
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.cody = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      librewolf
+      brave
+      tree
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    gnupg
+    git
+    gh
+    pass
+    tmux
+    curl
+    kitty
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
