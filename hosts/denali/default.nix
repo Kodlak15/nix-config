efe15b507ed9aa08230a9851e87f70f87d618f12
Kodlak15 <stanlcod15@gmail.com>
2023-11-02 16:35:39 -0700

    I fix bug

--- hosts/denali/default.nix
+++ hosts/denali/default.nix
@@ -131,7 +131,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   sound.enable = true;
-  # hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
 
   virtualisation = {
     docker.enable = true;
